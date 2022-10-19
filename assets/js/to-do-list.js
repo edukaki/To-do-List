62d2e6feca2b2a91f69a6524af005c2c539e2092
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-10-19 18:38:20 +0900

    Creating function that adds new element toDo

---
+++ assets/js/to-do-list.js
@@ -0,0 +1,39 @@
+const toDoForm = document.querySelector('[data-form="to-do__form"]');
+const toDoList = document.querySelector('[data-list="body"]');
+const toDoListArr = [];
+
+class ToDoItem {
+    id = 0;
+    checked = false;
+    text = "";
+
+    constructor(text){
+        this.text = text;
+        this.id++;
+    }
+}
+
+toDoForm.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    const newToDo = new ToDoItem(toDoForm.toDoInput.value);
+    toDoListArr.push(newToDo)
+    createNewItem(newToDo);
+    console.log(toDoListArr);
+
+})
+
+function createNewItem(element){
+    const newLi = document.createElement("li");
+    newLi.classList.add("c-to-do__container","c-to-do__container--spaceBetween");
+    newLi.setAttribute("data-listid",element.id)
+    toDoList.appendChild(newLi);
+    newLi.innerHTML = `
+            <label class="c-checkbox__container">
+                <input type="checkbox" class="checkbox--hide">
+                <span class="checkmark"></span>
+            </label>
+            <p class="c-to-do__text">${element.text}</p>
+            <button class="button--bgTransparent" name="btn_add"><img src="./assets/images/icon-cross.svg" alt="Cross Icon" data-crossid=${element.id}></button>
+            `;
+}
