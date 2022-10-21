2384d3102e28c33917401ea10199ebf795ab9f5f
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-10-22 08:59:24 +0900

    Fixing bugs where new elements didn't have the theme toggled

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,7 +8,5 @@ iconTheme.addEventListener("click", () => {
 
 function toggleTheme(){
     const body = document.body;
-    
     body.classList.toggle("body--dark-mode");
-    iconTheme.classList.toggle("-th-sun");
 }
